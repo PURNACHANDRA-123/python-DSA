7908fffc5ce534c543f237d1fdc2fdccc18053c1
purna chandra <purnachandrabehera615@gmail.com>
2024-09-29 15:37:14 +0530

    dsa python

---
+++ 1bigonoation.py
@@ -0,0 +1,21 @@
+#bionoation
+#bionoation is a sorting algorithm
+# write o(1) program
+#write o(n) program
+a=[1,2,3,4,5,6,7,8,9,10]
+
+for i in range(len(a)):
+    print(a[i])
+
+#we can calculate space complexity time complexity
+# o(n**2)
+# o(nlogn) #less time
+# o(n)
+# o(1)
+
+#in time complexity we consider fastest growing term
+#constants will be dropped
+
+
+
+
---
+++ 2array.py
@@ -0,0 +1,24 @@
+#in python list is implemented as dyanamic array
+#in python statistic array is not possible
+
+#1D array
+D1=[1,2,3,4,5,6,7,8,9,10]
+
+#2D array
+D2=[[1,2,3],[4,5,6],[7,8,9]]
+
+#3D array
+D3=[[[1,2,3],[4,5,6],[7,8,9]],[[10,11,12],[13,14,15],[16,17,18]]]
+
+
+print(D1)
+print(D2)
+print(D3)
+
+
+
+
+
+
+
+
